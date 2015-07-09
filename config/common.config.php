ed660a0b948a5d484797595f5534f00808e983ad
Jerry Ablan <jerryablan@dreamfactory.com>
2015-07-09 12:35:43 -0400

    Changed metadata key to "audit" in config

--- config/common.config.php
+++ config/common.config.php
@@ -144,7 +144,7 @@ if ( false !== ( $_managed = Enterprise::isManagedInstance() ) )
     $_storageBasePath = $_storagePath = Enterprise::getStoragePath();
     $_privatePath = Enterprise::getPrivatePath();
     $_ownerPrivatePath = Enterprise::getOwnerPrivatePath();
-    $_metadata = Enterprise::getConfig( 'env', [] );
+    $_metadata = Enterprise::getConfig( 'audit', [] );
 
     $_identity = array(
         'dsp.storage_id'         => $_instanceName,
